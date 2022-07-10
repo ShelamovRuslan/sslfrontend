907ba28ada93f95350018ef7fb36de4e5796ddf8
Shelamov Ruslan <shelamov.ruslan@gmail.com>
2022-07-10 06:36:25 +0300

    1

---
+++ src/main/java/com/example/sslfrontend/SslfrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sslfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslfrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SslfrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sslfrontend/SslfrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sslfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SslfrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
